a9b46d7dd29daff066b9e9955049778f10fa3d59
chaddesch <chaddesch4@gmail.com>
2016-10-29 17:46:04 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,34 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by Chad on 10/27/2016.
+ */
+
+public class Earthquake {
+    private String mMagnitude;
+
+    private String mLocation;
+
+    private String mDate;
+
+
+    public Earthquake(String magnitude, String location, String date) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mDate = date;
+    }
+
+    public String getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+
+}
